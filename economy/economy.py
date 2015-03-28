79056bcff1cd25170aef85a4654b92494de00a1b
Ezequiel Pozzo <ezequiel.pozzo@gmail.com>
2015-03-28 01:17:16 -0300

    Added economy classplaceholder and test.

---
+++ economy/economy.py
@@ -0,0 +1,15 @@
+class Economy():
+	def __init__(self, products):
+		pass
+
+	def add_market(self, name, **inventory):
+		pass
+
+	def add_route(self, name, from = None, to = None, traffic = None): #traffic? percentage? probability?
+		pass
+
+	def step(self):
+		pass
+
+	def __contains__(self, element):
+		pass
