3b161c8341f7eb1b2d35d8ebdf6fdc536ecaf0d2
Ezequiel Pozzo <ezequiel.pozzo@gmail.com>
2015-03-30 18:20:52 -0300

    Added 2D sandbox using cocos2D and Wesnoth tiles.

---
+++ sandbox/cocosExtension/tiles.py
@@ -0,0 +1,64 @@
+from cocos import tiles
+from math import floor
+
+class RegularHexCell(tiles.HexCell):
+    def __init__(self, i, j, height, properties, tile):
+        width = height
+        tiles.Cell.__init__(self, i, j, width, height, properties, tile)
+
+class RegularHexMap(tiles.HexMap):
+    def get_key_at_pixel(self, x, y):
+        """returns the grid coordinates for the hex that covers the point (x, y)
+
+        Reference:
+            Hexagonal grid math, by Ruslan Shestopalyuk
+            http://blog.ruslans.com/2011/02/hexagonal-grid-math.html
+        """
+        radius = self.edge_length
+        side = (self.tw * 3) // 4
+        height = self.th
+
+        ci = int(floor(x / side))
+        cx = int(x - side*ci)
+
+        ty = int(y - (ci % 2) * height / 2.0)
+        cj = int(floor(1.0 * ty / height))
+        cy = ty - height * cj
+
+        if cx <= abs(radius/2.0 - radius*cy/height):
+            cj = cj + (ci % 2) - (1 if (cy < height / 2.0) else 0)
+            ci = ci - 1
+        return ci, cj
+
+class RegularHexMapLayer(RegularHexMap, tiles.MapLayer):
+    def __init__(self, id, th, cells, origin=None, properties=None):
+        RegularHexMap.__init__(self, id, th, cells, origin, properties)
+        tiles.MapLayer.__init__(self, properties)
+
+def truncated_hexmap_factory(resource, tag):
+    height = int(tag.get('tile_height'))
+    width = height
+    origin = tag.get('origin')
+    if origin:
+        origin = map(int, tag.get('origin').split(','))
+    id = tag.get('id')
+
+    # now load the columns
+    cells = []
+    for i, column in enumerate(tag.getiterator('column')):
+        c = []
+        cells.append(c)
+        for j, cell in enumerate(column.getiterator('cell')):
+            tile = cell.get('tile')
+            if tile:
+                tile = resource.get_resource(tile)
+            else:
+                tile = None
+            properties = tiles._handle_properties(tag)
+            c.append(RegularHexCell(i, j, height, properties, tile))
+
+    properties = tiles._handle_properties(tag)
+    m = RegularHexMapLayer(id, height, cells, origin, properties)
+    resource.add_resource(id, m)
+
+    return m
---
+++ sandbox/cocosMap.py
@@ -0,0 +1,72 @@
+import cocosExtension.tiles as newtiles
+import cocos, pyglet
+
+cocos.tiles.Resource.register_factory('regularhexmap')(newtiles.truncated_hexmap_factory)
+
+#class Peasant(cocos.sprite.Sprite):
+#	def __init__(self):
+#		super( Peasant, self).__init__("assets/peasant.png")
+#
+#	def go_to_cell(self, cell):
+#		self.position = cell.center
+class DraggableScrollingManager(cocos.layer.ScrollingManager):
+	is_event_handler = True
+	def __init__(self):
+		super(DraggableScrollingManager, self).__init__()
+
+		self.start_drag = None
+		self.start_focus = None
+
+	def on_mouse_release(self, x, y, buttons, modifiers):
+		if buttons & pyglet.window.mouse.LEFT:
+			self.start_drag = None
+			self.start_focus = None
+
+	def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+		if buttons & pyglet.window.mouse.LEFT:
+			if not self.start_drag:
+				self.start_drag = (x, y)
+				self.start_focus = self.restricted_fx, self.restricted_fy
+			
+			self.set_focus(self.start_focus[0]+self.start_drag[0]-x, self.start_focus[1]+self.start_drag[1]-y)
+
+
+class Sandbox(cocos.layer.ColorLayer):
+	is_event_handler = True
+	def __init__(self):
+		super( Sandbox, self ).__init__( 20,20,20,255)
+		
+		r = cocos.tiles.load('map1.xml')
+		level1 = r['level1']
+
+		manager = DraggableScrollingManager()
+		manager.add(level1)
+		self.manager = manager
+		self.add(manager)
+
+		level1.set_view(0, 0, level1.px_width, level1.px_height)
+		self.current_map = level1
+
+		menu = cocos.layer.ColorLayer(20, 20, 20, 128, 50, 50)
+		menu.position = (50, 50)
+		self.add(menu)
+
+	#	self.peasant = Peasant()
+	#	self.peasant.go_to_cell(level1.get_cell(1, 1))
+
+	#	self.add(self.peasant, z=1)
+	def on_mouse_press(self, x, y, buttons, modifiers):
+		if buttons & pyglet.window.mouse.LEFT:
+			print(self.manager.pixel_from_screen(*cocos.director.director.get_virtual_coordinates(x, y)))
+			print(self.current_map.get_at_pixel(*self.manager.pixel_from_screen(*cocos.director.director.get_virtual_coordinates(x, y))))
+
+	def on_mouse_release(self, x, y, buttons, modifiers):
+		pass
+
+		
+	
+
+if __name__ == '__main__':
+	cocos.director.director.init(do_not_scale = True)
+	main_scene = cocos.scene.Scene (Sandbox())
+	cocos.director.director.run(main_scene)
